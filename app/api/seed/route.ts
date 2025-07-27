191ac46086b7121dc09c48ad0a87ac2f17163eaf
ped-suzanto <ped.travlr@gmail.com>
2025-07-27 20:09:41 +0800

    Update route.ts

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -3,9 +3,9 @@ import { db, products } from 'lib/db';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  return Response.json({
-    message: 'Uncomment to seed data after DB is set up.'
-  });
+  // return Response.json({
+  //   message: 'Uncomment to seed data after DB is set up.'
+  /// });
 
   await db.insert(products).values([
     {
